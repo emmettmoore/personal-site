f9d8813223e53d1dcf86d61f8613e4b7f9219d29
Emmett Moore <ewmoore823@gmail.com>
2018-02-28 23:36:40 -0500

    make tab for d3 visualizations

--- ebase/main/urls.py
+++ ebase/main/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^8a_viz/$', views.climbing_viz, name="climbing_viz"),
+    url(r'^d3/$', views.d3, name="d3"),
 ]
--- ebase/main/views.py
+++ ebase/main/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('hello world')
+    return HttpResponse(request, 'main/index.html', {})
 
 
 def climbing_viz(request):
     return render(request, 'main/climbing_viz.html', {})
+
+def d3(request):
+    return render(request, 'main/d3.html', {})
