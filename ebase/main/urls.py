43d90f3c3c9415594219e64fe7071828d979d3dc
Emmett Moore <ewmoore823@gmail.com>
2018-03-03 20:25:37 -0500

    rework top bar, and add framework for other tabs

--- ebase/main/urls.py
+++ ebase/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^8a_viz/$', views.climbing_viz, name="climbing_viz"),
     url(r'^d3/$', views.d3, name="d3"),
+    url(r'^resume/$', views.resume, name="resume"),
 ]
--- ebase/main/views.py
+++ ebase/main/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(request, 'main/index.html', {})
+    return render(request, 'main/index.html', {})
 
 
 def climbing_viz(request):
     return render(request, 'main/climbing_viz.html', {})
 
+
 def d3(request):
     return render(request, 'main/d3.html', {})
+
+
+def resume(request):
+    return render(request, 'main/resume.html', {})
