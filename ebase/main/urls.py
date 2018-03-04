1cda51c367969cce0775739860c2743843500290
Emmett Moore <ewmoore823@gmail.com>
2018-03-04 16:16:07 -0500

    add github png; rework colors and add bio

--- ebase/main/urls.py
+++ ebase/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
     url(r'^8a_viz/$', views.climbing_viz, name="climbing_viz"),
     url(r'^d3/$', views.d3, name="d3"),
     url(r'^resume/$', views.resume, name="resume"),
