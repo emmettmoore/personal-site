a860214aa6a5a1fba911ba1aa70a430d10f81bea
Emmett Moore <ewmoore823@gmail.com>
2018-03-04 17:01:30 -0500

    add gulpfile; start using scss

---
+++ ebase/main/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var production = false;
+
+gulp.task('default', ['sass']);
+
+var sass_task = function(sass_src, sass_dest) {
+    return function () {
+        return gulp.src(sass_src)
+            .pipe(gulpif(!production, sourcemaps.init()))
+            .pipe(sass({ outputStyle: production ? 'compressed' : 'nested' }))
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulpif(!production, sourcemaps.write('./maps')))
+            .pipe(gulp.dest(sass_dest))
+    }
+};
+
+gulp.task('sass', sass_task('./static/main/scss/*.scss', './static/main/build/css/'));
