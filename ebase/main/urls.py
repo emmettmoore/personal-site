68612129f03725b5b648e222352911e990aac7aa
Emmett Moore <ewmoore823@gmail.com>
2018-03-07 20:31:40 -0500

    cleanup

--- ebase/main/urls.py
+++ ebase/main/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^8a_viz/$', views.climbing_viz, name="climbing_viz"),
-    url(r'^d3/$', views.d3, name="d3"),
-    url(r'^resume/$', views.resume, name="resume"),
+    url(r'^timeline/$', views.timeline, name="timeline"),
 ]
--- ebase/main/views.py
+++ ebase/main/views.py
@@ -9,9 +9,5 @@ def climbing_viz(request):
     return render(request, 'main/climbing_viz.html', {})
 
 
-def d3(request):
-    return render(request, 'main/d3.html', {})
-
-
-def resume(request):
-    return render(request, 'main/resume.html', {})
+def timeline(request):
+    return render(request, 'main/timeline.html', {})
