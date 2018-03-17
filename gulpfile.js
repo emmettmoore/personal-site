36ff3c013f407b7e4413abd9da952a0ce4f94e2e
Emmett Moore <ewmoore823@gmail.com>
2018-03-17 14:04:52 -0400

    coffee build in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
+var gcoffee = require('gulp-coffee');
+var gutil = require('gulp-util');
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var production = false;
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'coffee']);
+
+gulp.task('coffee', function() {
+    gulp.src('./main/static/main/coffee/*.coffee')
+        .pipe(gulpif(!production, sourcemaps.init()))
+        .pipe(gcoffee({bare:true}).on('error', gutil.log))
+        .pipe(gulpif(!production, sourcemaps.write('./maps')))
+        .pipe(gulp.dest('./main/static/main/build/js/'))
+});
 
 var sass_task = function(sass_src, sass_dest) {
     return function () {
